15bd0ad14b4223e6c4a5779ba58163a528011e17
Maciej Rymarz <maciej.rymarz@escolasoft.com>
2023-07-31 16:22:21 +0200

    WIP

--- src/Providers/AuthServiceProvider.php
+++ src/Providers/AuthServiceProvider.php
@@ -3,18 +3,13 @@
 namespace EscolaLms\PencilSpaces\Providers;
 
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
-use Laravel\Passport\Passport;
 
 class AuthServiceProvider extends ServiceProvider
 {
     protected $policies = [];
 
-    public function boot()
+    public function boot(): void
     {
         $this->registerPolicies();
-
-        if (!$this->app->routesAreCached() && method_exists(Passport::class, 'routes')) {
-            Passport::routes();
-        }
     }
 }
